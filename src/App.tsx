21ff853db54899345c9ea717ccbac96fdef07e88
krups88 <b88pl88@gmail.com>
2023-02-19 21:52:37 +0100

    Teg

--- src/App.tsx
+++ src/App.tsx
@@ -10,4 +10,30 @@ const App = () => {
   );
 }
 
+function Rating () {
+  return(
+    <div>
+      <div>start</div>
+      <div>start</div>
+      <div>start</div>
+      <div>start</div>
+      <div>start</div>
+    </div>
+  );
+}
+
+function Accordion () {
+  return(
+    <div>
+      <h3>start</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+}
+
+
 export default App;
